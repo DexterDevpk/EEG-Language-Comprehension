e576c6fe54177d024dc36b10bb795714d30961e2
DexterDev <ymksheikh@gmail.com>
2025-05-17 14:19:06 +0500

    Update app.py

--- app.py
+++ app.py
@@ -195,4 +195,4 @@ def get_native_language():
         return jsonify({'native_language': native_language.capitalize(), 'message': ''})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
